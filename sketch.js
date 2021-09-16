fe29aa7e0121c59c0520fe644fd6779bb9b1c38c
Stella <chiara.poma@mail.polimi.it>
2021-09-16 14:45:41 +0200

    Chiara_Poma_consegna00
    Vorrei sapere se è possibile avere una revisione rispetto alla consegna perchè il risultato non è esattamente quello che volevo.

--- sketch.js
+++ sketch.js
@@ -4,10 +4,71 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
+  background("black");
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  noFill();
+  if (mouseIsPressed === true) {
+    let myColor = lerpColor(color("red"), color("yellow"), frameCount / 120);
+
+    if (frameCount >= 120) {
+      stroke(myColor);
+    }
+
+    let myColor1 = lerpColor(color("yellow"), color("green"), frameCount / 120);
+    if (frameCount >= 240) {
+      stroke(myColor1);
+    }
+
+    let myColor2 = lerpColor(color("green"), color("blue"), frameCount / 120);
+    if (frameCount >= 360) {
+      stroke(myColor2);
+    }
+
+    let myColor3 = lerpColor(color("blue"), color("red"), frameCount / 120);
+    if (frameCount >= 480) {
+      stroke(myColor3);
+    }
+
+    let myColor4 = lerpColor(color("red"), color("black"), 1);
+    if (frameCount >= 600) {
+      stroke(myColor4);
+
+      if (frameCount >= 840) {
+        noLoop();
+        draw();
+      }
+    }
+
+    translate(width / 2, height / 2);
+    rotate(frameCount * 3);
+
+    circle(260, 1, 3);
+    circle(260, 0, 3);
+    circle(260, 0, 10);
+    circle(250, 10, 30);
+    circle(220, 0, 3);
+    circle(200, 0, 10);
+
+    circle(180, 0, 40);
+    circle(170, 0, 10);
+    circle(160, 0, 3);
+    circle(150, 0, 3);
+    circle(130, 10, 5);
+    circle(120, 0, 10);
+    circle(120, 0, 20);
+
+    circle(100, 0, 1);
+    circle(80, 0, 20);
+    circle(70, 0, 9);
+    circle(50, 0, 15);
+
+    circle(30, 10, 5);
+    circle(30, 0, 1);
+    circle(20, 5, 1);
+    circle(10, 0, 5);
+    circle(0, 0, 3);
+  }
 }
